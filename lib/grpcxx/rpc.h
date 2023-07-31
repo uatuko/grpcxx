c381bd27bc6b86a4cb50c79130040e5436f2b2c8
Uditha Atukorala <ua@nuked.zone>
2023-07-31 02:32:37 +0100

    (grpcxx) use a map to keep track of service rpc handlers

--- lib/grpcxx/rpc.h
+++ lib/grpcxx/rpc.h
@@ -1,11 +1,56 @@
 #pragma once
 
+#include <functional>
+#include <optional>
+#include <string>
+
 #include "fixed_string.h"
+#include "status.h"
 
 namespace grpcxx {
 template <fixed_string M, typename T, typename U> struct rpc {
 	static constexpr std::string_view method{M};
+
 	using request_type  = T;
 	using response_type = U;
+
+	using optional_response_type = std::optional<response_type>;
+	using result_type            = std::pair<status, optional_response_type>;
+
+	std::function<result_type(const request_type &)> fn = [](const auto &) {
+		return result_type(status::code_t::unimplemented, std::nullopt);
+	};
+
+	request_type map(const std::string &data) const {
+		constexpr bool can_map = requires(request_type t) {
+			{ t.ParseFromString(std::declval<const std::string &>()) } -> std::same_as<bool>;
+		};
+		static_assert(can_map, "No known method to deserialize data");
+
+		request_type req;
+		if (!req.ParseFromString(data)) {
+			throw std::runtime_error("Failed to deserialize data");
+		}
+
+		return req;
+	}
+
+	std::string map(const optional_response_type &res) const {
+		constexpr bool can_map = requires(response_type t) {
+			{ t.SerializeToString(std::declval<std::string *>()) } -> std::same_as<bool>;
+		};
+		static_assert(can_map, "No known method to serialize data");
+
+		if (!res) {
+			return {};
+		}
+
+		std::string data;
+		if (!res->SerializeToString(&data)) {
+			throw std::runtime_error("Failed to serialize data");
+		}
+
+		return data;
+	}
 };
 } // namespace grpcxx
--- lib/grpcxx/service.h
+++ lib/grpcxx/service.h
@@ -1,69 +1,90 @@
 #pragma once
 
-#include <optional>
 #include <string>
 #include <string_view>
 #include <tuple>
+#include <unordered_map>
 
 #include "status.h"
 
 namespace grpcxx {
 namespace concepts {
 template <typename T>
-concept rpc_type = requires(T) {
+concept rpc_type = requires(T t) {
+	// Method
 	T::method;
-	{std::same_as<decltype(T::method), std::string_view>};
+	requires std::same_as<std::remove_cv_t<decltype(T::method)>, std::string_view>;
+
+	// Request
 	typename T::request_type;
+	{ t.map(std::declval<const std::string &>()) } -> std::same_as<typename T::request_type>;
+
+	// Response
 	typename T::response_type;
+	typename T::optional_response_type;
+	{
+		t.map(std::declval<const typename T::optional_response_type &>())
+		} -> std::same_as<std::string>;
+
+	// Handler
+	{t.fn};
+	requires std::same_as < decltype(t.fn),
+		std::function <
+			std::pair<status, typename T::optional_response_type>(const typename T::request_type &)
+	>> ;
 };
 } // namespace concepts
 
 template <concepts::rpc_type... R> class service {
 public:
-	using response_t = std::pair<status, std::string>;
+	using data_t     = std::string;
+	using response_t = std::pair<status, data_t>;
 
-	response_t call(std::string_view method, const std::string &data) {
-		response_t response(status::code_t::unimplemented, {});
-		dispatch([&](const auto &arg) -> bool {
-			if (method != arg.method) {
-				return false;
-			}
+	using handler_t  = std::function<response_t(const data_t &)>;
+	using handlers_t = std::unordered_map<std::string_view, handler_t>;
 
-			using rpc_t = std::remove_reference_t<decltype(arg)>;
-			typename rpc_t::request_type req;
-			if (!req.ParseFromString(data)) {
-				throw std::runtime_error("Failed to parse proto data");
-			}
-
-			auto res       = rpc<typename rpc_t::request_type, typename rpc_t::response_type>(req);
-			response.first = res.first;
-
-			if (res.second) {
-				std::string out;
-				if (!res.second->SerializeToString(&out)) {
-					throw std::runtime_error("Failed to serialise proto data");
-				}
-
-				response.second = out;
-			}
-
-			return true;
-		});
-
-		return response;
+	constexpr service() {
+		std::apply([&](auto &&...args) { (make_handler(args), ...); }, _methods);
 	}
 
-	template <typename T, typename U> std::pair<status, std::optional<U>> rpc(const T &t) {
-		return {status::code_t::unimplemented, std::nullopt};
+	response_t call(std::string_view method, const std::string &data) {
+		auto it = _handlers.find(method);
+		if (it == _handlers.end()) {
+			return {status::code_t::not_found, {}};
+		}
+
+		return it->second(data);
+	}
+
+	template <fixed_string M> constexpr void rpc(const auto &fn) {
+		constexpr bool found = ((R::method == M) || ...);
+		static_assert(found, "Method not found in service");
+
+		std::apply([&](auto &&...args) { (rpc_helper<M>(args, fn) || ...); }, _methods);
 	}
 
 private:
-	template <typename F> bool dispatch(F &&f) {
-		return std::apply(
-			[&f](const auto &...args) { return (bool{std::forward<F>(f)(args)} || ...); },
-			_methods);
+	constexpr void make_handler(const auto &rpc) {
+		auto handler = [&rpc](const data_t &data) -> response_t {
+			auto req    = rpc.map(data);
+			auto result = rpc.fn(req);
+
+			return {result.first, rpc.map(result.second)};
+		};
+
+		_handlers.insert({rpc.method, handler});
 	}
 
+	template <fixed_string M> constexpr bool rpc_helper(auto &&rpc, auto &&fn) {
+		if constexpr (M == std::remove_reference_t<decltype(rpc)>::method) {
+			rpc.fn = fn;
+			return true;
+		}
+
+		return false;
+	}
+
+	handlers_t       _handlers;
 	std::tuple<R...> _methods;
 };
 } // namespace grpcxx
